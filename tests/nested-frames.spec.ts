af5667f7ed1ef7bf870d2cd5269795ae5130247f
Paul Doherty <19390381+PaulDot@users.noreply.github.com>
2026-08-16 17:46:10 +0100

    test: adding nested iframes page test

---
+++ tests/nested-frames.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+import { NestedFramesPage } from '../pages/nested-frames';
+
+test.describe('Nested Frames Sandbox', () => {
+    let nestedFramesPage: NestedFramesPage;
+
+    test.beforeEach(async ({ page }) => {
+        nestedFramesPage = new NestedFramesPage(page);
+        await nestedFramesPage.navigate();
+    });
+
+    test.only('should successfully read text from nested html frames', async () => {
+        const leftBody = nestedFramesPage.getLeftFrameBody();
+        await expect(leftBody).toHaveText('LEFT');
+        const middleBody = nestedFramesPage.getMiddleFrameBody();
+        await expect(middleBody).toHaveText('MIDDLE');
+        const rightBody = nestedFramesPage.getRightFrameBody();
+        await expect(rightBody).toHaveText('RIGHT');
+        const bottomBody = nestedFramesPage.getBottomFrameBody();
+        await expect(bottomBody).toHaveText('BOTTOM');
+    })
+})
